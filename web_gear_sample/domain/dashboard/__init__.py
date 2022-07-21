c12fee85e1989e4f8170dea73563d9c33b814efa
Marlos Ribeiro <marlos.mgrb@gmail.com>
2022-07-21 01:27:36 -0300

    :lipstick:  Adding templates pages

--- web_gear_sample/domain/dashboard/__init__.py
+++ web_gear_sample/domain/dashboard/__init__.py
@@ -2,11 +2,11 @@
 Blueprint to base web app interface
 """
 from flask import Flask
-from .bp_index import bp
+from .dashboard import dashboard_bp
 
 
 def init_app(app: Flask):
     """
     Blueprints register
     """
-    app.register_blueprint(bp)
+    app.register_blueprint(dashboard_bp)
---
+++ web_gear_sample/domain/dashboard/dashboard.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+
+dashboard_bp = Blueprint('dashboard', __name__, template_folder='templates')
+
+
+@dashboard_bp.route('/')
+def main_page():
+    """
+    Render template
+    """
+    return render_template('dashboard.html')
